a9b733189a752772e8c5842e4e6603ceb23cdf3f
sujang958 <enter7377@naver.com>
2021-11-20 14:06:21 +0900

    feat: dynamic survey answers

--- app.ts
+++ app.ts
@@ -7,7 +7,7 @@ import util from "./util/index";
         const { browser, page } = await util.createBrowser()
         await util.selectSchool(page, school)
         await util.putSutdentInfo(page, student)
-        await util.diagnosis(page)
+        await util.diagnosis(page, ["1", "1", "1", "1"]) // 1 = no; 2 = yes
         await browser.close()
     } catch (e) {
         console.log(e)
--- util/diagnosis.ts
+++ util/diagnosis.ts
@@ -1,15 +1,18 @@
 import { Page } from "playwright"
 
-const diagnosis = async (page: Page) => {
-    await page.waitForURL('https://hcs.eduro.go.kr/#/main', {waitUntil: 'domcontentloaded'})
-    await page.click('.memberWrap > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > span:nth-child(1)')
-    await page.waitForSelector('#survey_q3a1')
-    await page.click('#survey_q1a1')
-    await page.click('#survey_q2a1')
-    await page.click('#survey_q3a1')
-    await page.click('#btnConfirm')
-    await page.waitForSelector('.guide_center > p:nth-child(2)')
-    await page.screenshot({path: 'diagnosis.jpg'})
+const diagnosis = async (page: Page, survey: string[]) => {
+  await page.waitForURL("https://hcs.eduro.go.kr/#/main", {
+    waitUntil: "domcontentloaded",
+  })
+  await page.click(
+    ".memberWrap > div:nth-child(2) > ul:nth-child(1) > li:nth-child(1) > a:nth-child(1) > span:nth-child(1)"
+  )
+  await page.waitForSelector("#survey_q3a1")
+  for (const i in survey)
+    await page.click(`#survey_q${Number(i) + 1}a${survey[i]}`)
+  await page.click("#btnConfirm")
+  await page.waitForSelector(".guide_center > p:nth-child(2)")
+  await page.screenshot({ path: "diagnosis.jpg" })
 }
 
 export default diagnosis
