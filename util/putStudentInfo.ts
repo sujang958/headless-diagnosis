73daffd5f03c964ad6ae5e0029d26795872bac74
unknown <enter7377@naver.com>
2022-02-23 23:20:55 +0900

    fix: fix didn't work

--- util/putStudentInfo.ts
+++ util/putStudentInfo.ts
@@ -1,24 +1,24 @@
 import { Page } from "playwright"
 
 interface Student {
-    name: string
-    birth: string
-    pw: string
+  name: string
+  birth: string
+  pw: string
 }
 
 var window: any = {}
 
 const putSutdentInfo = async (page: Page, student: Student) => {
-    await page.fill('#user_name_input', student.name)
-    await page.fill('#birthday_input', student.birth)
-    await page.click('#btnConfirm')
-    await page.waitForTimeout(1000)
-    await page.dispatchEvent('#password', 'focus')
-    await page.waitForTimeout(200)
-    for (const part of student.pw)
-        await page.click(`a[aria-label="${part}"]`)
-    await page.click('#btnConfirm')
-    await page.waitForLoadState()
+  await page.fill("#user_name_input", student.name)
+  await page.fill("#birthday_input", student.birth)
+  await page.click("#btnConfirm")
+  await page.waitForTimeout(1000)
+  console.log("asdf")
+  await page.click("#WriteInfoForm > table > tbody > tr > td > div > button")
+  await page.waitForTimeout(200)
+  for (const part of student.pw) await page.click(`a[aria-label="${part}"]`)
+  await page.click("#btnConfirm")
+  await page.waitForLoadState()
 }
 
 export default putSutdentInfo
