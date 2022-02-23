2c42b3cecb67976035833721343a69fe16f8f99c
unknown <enter7377@naver.com>
2022-02-24 00:52:44 +0900

    perf: improve time threshold

--- app.ts
+++ app.ts
@@ -2,8 +2,6 @@ import students from "./students.json"
 import Diagnosis from "./util/diagnosis"
 
 const diagnosis = async () => {
-  // Korea's UTC difference is + 9
-  if (new Date().getUTCHours() + 9 > 6) return
   students.map(async ({ student, school }) => {
     try {
       const diagnosis = new Diagnosis(student, school)
@@ -17,6 +15,11 @@ const diagnosis = async () => {
 }
 
 diagnosis()
-// setInterval(diagnosis, 1000 * 60 * 5) // this is for loop
+setInterval(() => {
+  const KoreaHours = new Date().getUTCHours() + 9
+  if (KoreaHours > 24) {
+    if (KoreaHours - 24 < 7) diagnosis()
+  } else if (KoreaHours < 7) diagnosis()
+}, 1000 * 60 * 5) // this is for loop
 
 process.on("uncaughtException", (e) => console.log(e))
